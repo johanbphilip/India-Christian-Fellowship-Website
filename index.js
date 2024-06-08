05eeab74a87c15e6ad8b1eff36513d11b34ac2be
Johan <johanbp27@gmail.com>
2024-06-08 12:17:42 -0400

    root folder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let map;
 async function initMap() {
   // The location of ICF
   const icf = {lat: 43.70929638606898, lng: -79.59080664944702};
-  const properties = {zoom: 15, center: icf, mapId: "74c1b1b5509a4673", gestureHandling: 'greedy', disableDefaultUI: true};
+  const properties = {zoom: 14, center: icf, mapId: "74c1b1b5509a4673", gestureHandling: 'greedy', disableDefaultUI: true, draggableCursor: 'pointer', fullscreenControl: true};
   // Request needed libraries.
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
@@ -44,7 +44,7 @@ function headerColorChanger () {
   const scrollPosition = window.scrollY;
   const triggerHeight = 650;
   if (scrollPosition > triggerHeight) {
-    header.style.backgroundColor = "#28343e";
+    header.style.backgroundColor = "#1e2429";
   } else {
     header.style.backgroundColor = "transparent"
   }
